925bfdc4bdb3747dd5e663fb8b1c55e0aedde8d0
Utkarsh-02022007 <utkarshyadav669@gmail.com>
2026-01-21 15:05:21 +0530

    first commmit

---
+++ Practiceproblem/hello.js
@@ -0,0 +1,54 @@
+// ----event listener------ 
+
+// // const Start=document.querySelector(".Start")
+// // const Stop=document.querySelector(".Stop")
+
+// // function TimeSt(){
+// //     console.log("Time Started");
+// // }
+
+// // Start.addEventListener("click",TimeSt)
+// // Stop.addEventListener("click",()=>{
+// //     console.log("Timer Ended")
+// //     Start.removeEventListener("click",TimeSt)
+// // })
+// const Start=document.querySelector(".Start")
+
+// function key(event){
+//     console.log(event.key)
+//     console.log("Button is clicked")
+// }
+// Start.addEventListener("keyup",key)
+
+// const form=document.querySelector('form')
+// form.addEventListener('submit',(even)=>{
+//     console.log("Form Submitted")
+//     even.preventDefault();
+// })
+
+// -----------------event propogation------------ travelling of DOM. 3stages => capturing,target,bubbleing. Default Capturing True is used for bubbleing
+
+// const div2=document.querySelector(".div2")
+// const btn=document.querySelector("button")
+// const div=document.querySelector(".div")
+
+// div.addEventListener('click',(event)=>{
+//     console.log("div1 is clicked")
+//     event.preventDefault();
+// })
+// div2.addEventListener('click',(event)=>{
+//     console.log("div2 is clicked")
+//     event.preventDefault();
+// })
+// btn.addEventListener('click',(event)=>{
+//     console.log("btn is clicked")
+//     event.preventDefault();
+// })
+
+// let firstName="harshit";
+
+// console.log(firstName[firstName.length-3])
+// console.log(firstName.length
+
+
+
